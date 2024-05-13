0a3bcf113e25c483332d9253524984d561d9583b
Douglas Shinzato <shinzato.douglas@gmail.com>
2024-05-12 22:28:07 -0400

    navbar adjustments

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+// const mobileMenuButton = document.querySelector(".mobileMenuButton")
+// const navBar = document.querySelector(".nav-bar")
+// mobileMenuButton.addEventListener("click", () => {
+//   mobileMenuButton.classList.toggle("is-active")
+//   navBar.classList.toggle("is-active")
+// })
+// Add this script to your HTML file
 const mobileMenuButton = document.querySelector(".mobileMenuButton")
-const mobileMenu = document.querySelector(".mobile-menu")
+const navBarUl = document.querySelector(".nav-bar ul")
+
 mobileMenuButton.addEventListener("click", () => {
   mobileMenuButton.classList.toggle("is-active")
-  mobileMenu.classList.toggle("is-active")
+  navBarUl.classList.toggle("active")
 })
